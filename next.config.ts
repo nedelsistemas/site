ab50568d90a60e0060b1759a4523838f39538df4
Fernando <fernando@nedel.inf.br>
2026-05-29 16:21:55 -0300

    ajuste de autorização

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["http://10.1.1.38:3000"],
+  allowedDevOrigins: ["*"],
   output: "standalone",
 };
 
