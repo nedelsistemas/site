57d598e99dd6f8af22a3030aee3273a4794e85c3
Fernando Ames Hickmann <fernandoameshickmann@icloud.com>
2026-06-15 09:40:46 -0300

    chore: alterado modo de exportação

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["10.1.1.*"],
-  // output: "standalone",
+  output: "standalone",
 };
 
 export default nextConfig;
